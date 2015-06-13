a9cdf77f83dde9e3c947e9253be567eb96ea9153
Keith Olenchak <kenjin.domini@gmail.com>
2015-06-13 02:35:58 +0000

    added support for travis-ci

---
+++ test/eventPageTest.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+//var eventPage = require("../eventPage.js");
+
+describe("eventPage", function(){
+    describe("authorize", function() {
+        it("Has no logic to test.", function(){
+            //nothing to test.
+        })
+    })
+    
+    describe("gmailAPILoaded", function() {
+        it("Has no logic to test.", function() {
+            //nothing to test.
+        })
+    })
+})
