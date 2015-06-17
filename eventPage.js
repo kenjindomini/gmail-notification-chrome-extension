d9689affbc03f9391cff54dd5dc8ea0d66f0898a
Keith Olenchak <kenjin.domini@gmail.com>
2015-06-17 20:56:50 +0000

    laying down ground work for communications between content scripts and background script.

--- eventPage.js
+++ eventPage.js
@@ -1,20 +1,34 @@
 window.addEventListener("beforeunload", cleanUp, false);
+chrome.runtime.onMessage.addListener(messageHandler(request, sender, sendResponse));
+var CONFIGURATION = {
+    'pullInterval': 30000, //TODO: make timer configurable via popup.html.
+    'monitorLabels': [
+        'CATEGORY_PERSONAL',
+        'CATEGORY_SOCIAL',
+        'CATEGORY_FORUMS',
+        'CATEGORY_UPDATES'
+        ] //list should be configured via popup.html.
+}
 if (typeof document != 'undefined'){
 	var head = document.getElementsByTagName('head')[0];
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
 	script.src = "https://apis.google.com/js/client.js";
 	head.appendChild(script);
-	//TODO: move to own function called by button on popup.html.
-	//oauth2 auth
+}
+
+function authenticate() {
+    //oauth2 auth
 	chrome.identity.getAuthToken(
-		{'interactive': true},
+		{'interactive': true
+		},
 		function(){
+		    authorize();
 		}
 	);
 }
 function authorize(){
-  gapi.auth.authorize(
+	gapi.auth.authorize(
 		{
 			client_id: '107921446115-71iua4ttnpqf3l2ud11egvrnc6t3od7p.apps.googleusercontent.com',
 			immediate: true,
@@ -45,6 +59,8 @@ function gmailAPILoaded(){
     var topic;
     gapi.client.pubsub.projects.topics.create({
     	'name': topicName
+    	}).then(function(response) {
+    	  topic = response;  
     	});
     //Subscribe to the topic.
     var subscription;
@@ -54,6 +70,7 @@ function gmailAPILoaded(){
     }).then(function(response){
     	subscription = response;
     });
+    //Allow Gmail to publish messages to our topic.
     gapi.client.pubsub.projects.topics.setIamPolicy({
     	'resource': topicName,
     	'Request body': {
@@ -66,15 +83,18 @@ function gmailAPILoaded(){
     	}
     });
     //Tell api to publish notifications of new gmail messages to topic.
+    var watchResponse;
     gapi.client.gmail.users.watch({
     	'userId': 'me',
     	'Request body': {
-    		"topicName": topicName
-    		//A list of labelIds should be provided to api to restrict notifications. list should be configured via popup.html.
+    		"topicName": topicName,
+    		"labelIds": CONFIGURATION.monitorLabels
     	}
+    }).then(function(response){
+        watchResponse = response;
     });
     //poll topic every 30 seconds.
-    window.setInterval(function(){pullNotifications();}, 30000); //TODO: make timer configurable via popup.html.
+    window.setInterval(function(){pullNotifications();}, CONFIGURATION.pullInterval);
 }
 
 function pullNotifications() {
@@ -83,4 +103,41 @@ function pullNotifications() {
 
 function cleanUp(e) {
 	//TODO: Add code to clean up topics,mailbox watch, etc.
+	gapi.client.gmail.users.stop({
+	    'userId': 'me'
+	})
+}
+
+function getLabels() {
+    var labelList;
+    gapi.client.gmail.users.labels.list({
+        'userId': 'me'
+    }).then(function(response){
+        labelList = response;
+    });
+    return labelList;
+}
+
+function messageHandler(request, sender, sendResponse) {
+    if (request.action == "setConfig") {
+        setConfig(request.config);
+        sendResponse({action: request.action, status: "completed"});
+    }
+    if (request.action == "getLabels") {
+        var labelList = setLabels();
+        sendResponse({action: request.action, status: "completed", labels: labelList});
+    }
+    if (request.action == "authenticate") {
+        authenticate();
+        sendResponse({action: request.action, status: "completed"});
+    }
+}
+
+function setConfig(config) {
+    if (typeof config.pullInterval != 'undefined') {
+        CONFIGURATION.pullInterval = config.pullInterval;
+    }
+    if (typeof config.monitorLabels != 'undefined') {
+        CONFIGURATION.monitorLabels = config.monitorLabels;
+    }
 }
--- test/eventPageTest.js
+++ test/eventPageTest.js
@@ -1,6 +1,6 @@
-var expect = require('chai').expect;
-var assert = require('chai').assert;
-var eventPage = require("../eventPage.js");
+//var expect = require('chai').expect;
+//var assert = require('chai').assert;
+//var eventPage = require("../eventPage.js");
 
 describe("eventPage", function(){
     describe("authorize", function() {
