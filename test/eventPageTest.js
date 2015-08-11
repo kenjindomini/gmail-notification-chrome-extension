bc97b7b505365167675c9ada98de775da555de81
Keith Olenchak <kenjin.domini@gmail.com>
2015-08-11 17:40:50 +0000

    Restructured to ease writting unit tests.

--- test/eventPageTest.js
+++ test/eventPageTest.js
@@ -1,17 +1,41 @@
-//var expect = require('chai').expect;
-//var assert = require('chai').assert;
-//var eventPage = require("../eventPage.js");
+var vm = require("vm");
+var fs = require("fs");
+var chrome = require("sinon-chrome");
 
-describe("eventPage", function(){
-    describe("authorize", function() {
-        it("Has no logic to test.", function(){
-            //nothing to test.
-        })
-    })
+describe('event page', function() {
+    this.timeout(4000);
     
-    describe("gmailAPILoaded", function() {
-        it("Has no logic to test.", function() {
-            //nothing to test.
-        })
-    })
-})
+/*    it('should sync cloud CONFIGURATION to global var CONFIGURATION, if exists', function(done){
+        page.open('empty.html', function() {
+            page.evalulate(function(localStorage) {
+                chrome.storage.sync.get.withArgs('CONFIGURATION').yields(JSON.parse(localStorage));
+            }, fs.read('data/chrome.storage.sync.get-CONFIGURATION_good.json'));
+            page.injectJs('../src/eventPage.js');
+            page.evalulate(function() {
+                
+            });
+        });
+        done();
+    });*/
+    it('should sync global var CONFIGURATION to cloud CONFIGURATION, if it does not exist', function(done){
+        page.open('empty.html', function() {
+            page.evalulate(function(cloudStorage) {
+                chrome.storage.sync.get.withArgs('CONFIGURATION').yields(JSON.parse(cloudStorage));
+                chrome.storage.onChanged.trigger({});
+            }, fs.read('data/chrome.storage.sync.get-CONFIGURATION_bad.json'));
+            page.injectJs('../src/eventPage.js');
+            page.evalulate(function() {
+                sinon.assert.calledWithMatch(chrome.storage.sync.set, {'CONFIGURATION': {
+                    pullInterval: 30000,
+                    monitorLabels: [
+                        'CATEGORY_PERSONAL',
+                        'CATEGORY_SOCIAL',
+                        'CATEGORY_FORUMS',
+                        'CATEGORY_UPDATES'
+                    ]
+                }});
+            });
+        });
+        done();
+    });
+});
