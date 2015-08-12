875f5eb4b13ba088d1182582bc0bb143bd9d3f82
Keith Olenchak <kenjin.domini@gmail.com>
2015-08-12 13:58:19 +0000

    added chai.expect, renamed a variable

--- test/eventPageTest.js
+++ test/eventPageTest.js
@@ -1,22 +1,25 @@
 //var vm = require("vm");
 var fs = require("fs");
 var chrome = require("sinon-chrome");
+var expect = require('chai').expect;
 
 describe('event page', function() {
     this.timeout(4000);
     
-/*    it('should sync cloud CONFIGURATION to global var CONFIGURATION, if exists', function(done){
+    it('should sync cloud CONFIGURATION to global var CONFIGURATION, if exists', function(done){
         page.open('empty.html', function() {
-            page.evalulate(function(localStorage) {
-                chrome.storage.sync.get.withArgs('CONFIGURATION').yields(JSON.parse(localStorage));
+            var config;
+            page.evalulate(function(cloudStorage) {
+                config = JSON.parse(cloudStorage);
+                chrome.storage.sync.get.withArgs('CONFIGURATION').yields(config);
             }, fs.read('data/chrome.storage.sync.get-CONFIGURATION_good.json'));
             page.injectJs('../src/eventPage.js');
             page.evalulate(function() {
-                
+                expect(CONFIGURATION).to.equal(config);
             });
         });
         done();
-    });*/
+    });
     it('should sync global var CONFIGURATION to cloud CONFIGURATION, if it does not exist', function(done){
         page.open('empty.html', function() {
             page.evalulate(function(cloudStorage) {
@@ -38,4 +41,17 @@ describe('event page', function() {
         });
         done();
     });
+    
+    //test failed authentication causes '!' badgeText
+    
+    //test '!' is removed after interactive auth
+    
+    //test pullNotifications responds as expected to no new messages
+    
+    //test pullNotifications responds as expected with new messages
+    
+    //TODO: figure out how to test message handler
+    ////will involve testing setConfig and getLabels
+    
+    //stub and test gapi calls.
 });
