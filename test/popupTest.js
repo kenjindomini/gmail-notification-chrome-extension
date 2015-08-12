baf06dd3d3d3c400c6f4bcd088736aeeb6c68abc
Keith Olenchak <kenjin.domini@gmail.com>
2015-08-12 13:59:15 +0000

    set up shell for popup.js unit tests

---
+++ test/popupTest.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+var chrome = require("sinon-chrome");
+var expect = require('chai').expect;
+
+//test getLabels
+
+//test authorize success
+
+//test authorize fail
+
+//test createLabelList
