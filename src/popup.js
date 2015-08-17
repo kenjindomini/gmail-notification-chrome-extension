3d441aa59a84a9a47b312285031b9a4663ca9e8c
Keith Olenchak <kenjin.domini@gmail.com>
2015-08-17 13:04:36 +0000

    Linted

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,15 @@
+/*property
+    action, addEventListener, appendChild, authStatus, checked, config,
+    createElement, find, firstChild, foreach, getElementById, id, labels, log,
+    monitorLabels, name, pullInterval, querySelector, removeChild, runtime,
+    sendMessage, status, textContent, type, value
+*/
+/*global
+    chrome, window, console
+*/
+/*jslint
+    browser: true
+*/
 var CONFIGURATION = {
     pullInterval: 30000,
     monitorLabels: [
@@ -5,22 +17,23 @@ var CONFIGURATION = {
         'CATEGORY_SOCIAL',
         'CATEGORY_FORUMS',
         'CATEGORY_UPDATES'
-        ]
+    ]
 };
 var labelList;
-
 document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
     document.querySelector('#submitConfig').addEventListener('click', setConfig);
     var authStatus = getAuthStatus();
     getConfig();
-    if(authStatus == true) {
-    getLabels();
-    createLabelList();
+    if (authStatus === true) {
+        getLabels();
+        createLabelList();
     }
 });
 
 function getLabels() {
-    chrome.runtime.sendMessage({action: "getLabels"}, function(response){
+    'use strict';
+    chrome.runtime.sendMessage({action: "getLabels"}, function (response) {
         console.log('chrome.runtime.sendMessage({action: "getLabels"}) returned:');
         console.log(response);
         labelList = response.labels;
@@ -28,18 +41,18 @@ function getLabels() {
 }
 
 function authorize() {
-    chrome.runtime.sendMessage({action: "authenticate"}, function(response){
+    'use strict';
+    chrome.runtime.sendMessage({action: "authenticate"}, function (response) {
         console.log('chrome.runtime.sendMessage({action: "authenticate"}) returned:');
         console.log(response);
-        if (response.status == "success") {
+        if (response.status === "success") {
             var authorizeDiv = document.getElementById("authorizeDiv");
             while (authorizeDiv.firstChild) {
                 authorizeDiv.removeChild(authorizeDiv.firstChild);
             }
             getLabels();
             createLabelList();
-        } 
-        else {
+        } else {
             var authorizeText = document.getElementById("authorizeText");
             authorizeText.textContent = "Authorization was canceled or aborted. Authorization is required for this extension.";
         }
@@ -47,7 +60,8 @@ function authorize() {
 }
 
 function getConfig() {
-    chrome.runtime.sendMessage({action: "getConfig"}, function(response){
+    'use strict';
+    chrome.runtime.sendMessage({action: "getConfig"}, function (response) {
         console.log('chrome.runtime.sendMessage({action: "getConfig"}) returned:');
         console.log(response);
         CONFIGURATION = response.config;
@@ -57,18 +71,20 @@ function getConfig() {
 }
 
 function setConfig() {
-    //TODO collect config values from popup.html.
-    chrome.runtime.sendMessage({action: "setConfig", config: CONFIGURATION}, function(response){
+    'use strict';
+    //to do: collect config values from popup.html.
+    chrome.runtime.sendMessage({action: "setConfig", config: CONFIGURATION}, function (response) {
         console.log('chrome.runtime.sendMessage({action: "setConfig", config: CONFIGURATION}) returned:');
         console.log(response);
     });
 }
 
 function getAuthStatus() {
-    chrome.runtime.sendMessage({action: "getAuthStatus"}, function(response){
+    'use strict';
+    chrome.runtime.sendMessage({action: "getAuthStatus"}, function (response) {
         console.log('chrome.runtime.sendMessage({action: "getAuthStatus"}) returned:');
         console.log(response);
-        if(response.authStatus == false) {
+        if (response.authStatus === false) {
             createAuthorizeButton();
         }
         return response.authStatus;
@@ -76,20 +92,24 @@ function getAuthStatus() {
 }
 
 function createLabelList() {
+    'use strict';
     var labelListDiv = document.getElementById("labelList");
-    labelList.foreach(function(currentValue, index, array){
-       var checkbox = document.createElement("input");
-       checkbox.type = "checkbox";
-       checkbox.name = "Labels";
-       checkbox.value = currentValue.name;
-       if (CONFIGURATION.monitorLabels.find(currentValue.id) != 'undefined'){
-           checkbox.checked = true;
-       }
-       labelListDiv.appendChild(checkbox);
+    labelList.foreach(function (currentValue, index, array) {
+        console.log("labelList-foreach index = " + index + " array = ");
+        console.log(array);
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "Labels";
+        checkbox.value = currentValue.name;
+        if (CONFIGURATION.monitorLabels.find(currentValue.id) !== 'undefined') {
+            checkbox.checked = true;
+        }
+        labelListDiv.appendChild(checkbox);
     });
 }
 
 function createAuthorizeButton() {
+    'use strict';
     var authorizeDiv = document.getElementById("authorize");
     var authorizeText = document.createElement("p");
     authorizeText.name = "authorizeText";
